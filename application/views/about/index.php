87a94c5688e25063a58e79ada370c3c229010ef6
devaega13@gmail.com <devaega13@gmail.com>
2021-04-28 10:53:40 +0700

    Ci-App WPU

---
+++ application/views/about/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+	<center><h1 class= mt-4>About Me</h1></center>
+	<center>
+		<img src="<?= base_url(); ?>assets/img/ega.jpg" alt="Deva Ega Marinda" width="200" class="rounded-circle shadow">
+		<br>
+		<br>
+		<p>Halo, nama saya Deva Ega Marinda</p>
+		<p>NIM saya, 19.01.53.0001</p>
+		<p>Mata Kuliah Web Service</p>
+	</center>
+</div>
